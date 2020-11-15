710b3f7eadcd48a176a3d2f759bcadb44f37c2df
Daniel Carrasco <danielmadrid19@gmail.com>
2020-11-15 10:52:02 +0100

    Updated info in example files

--- examples/ArduMenu/ArduMenu.ino
+++ examples/ArduMenu/ArduMenu.ino
@@ -1,20 +1,14 @@
-#include "ArduMenu.h" // Menu class header
-#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_GFX.h> // Core graphics library
 #include <Adafruit_PCD8544.h> // Hardware-specific library
 #include <SPI.h>
+#include "ArduMenu.h" // Menu class header. Is important to load this library after the display library.
 
-// Software SPI (slower updates, more flexible pin options):
-// pin 7 - Serial clock out (SCLK)
-// pin 6 - Serial data out (DIN)
-// pin 5 - Data/Command select (D/C)
-// pin 4 - LCD chip select (CS)
-// pin 3 - LCD reset (RST)
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
 // Functions definitions must be done before the menu definition
-void redLedOn(MENU_ITEM);
-void redLedOff(MENU_ITEM);
-void allLedsOff(MENU_ITEM);
+bool redLedOn(MENU_ITEM);
+bool redLedOff(MENU_ITEM);
+bool allLedsOff(MENU_ITEM);
 bool greenLedToggle(bool);
 int whiteLedRange(int8_t);
 int whiteLedRangeOnExit(int8_t);
@@ -44,12 +38,11 @@ int whiteLedRangeOnExit(int8_t);
 //   drawMenu: Draw the current menu on the display
 //   down: On menu will select the next item. On range will call the callback function with 1 as argument.
 //   up: On menu will select the previous item. On range will call the callback function with -1 as argument.
-//   enter: Select the current menu item. On AM_ITEM_TYPE_COMMAND and AM_ITEM_TYPE_EOM, callback function will be called.
+//   enter: Select the current menu item. On AM_ITEM_TYPE_COMMAND and AM_ITEM_TYPE_EOM, callback function will be called. On AM_ITEM_TYPE_COMMAND if the callback function returns true, the top menu will be drawn.
 //                                        On AM_ITEM_TYPE_MENU the SubItems menu will be shown.
 //                                        On AM_ITEM_TYPE_RANGE, a range box will be shown. The callback function will be called with 0 as argument to retreive the current status. On exit function callback will be called with 2 as argument.
 //                                        On AM_ITEM_TYPE_TOGGLE, the callback function will be called with true as argument, and the returned bool will be used to redraw the icon.
-//   setSelectionMode: Se the selection mode between AM_SELECTION_MODE_ICON and AM_SELECTION_MODE_INVERTED. AM_SELECTION_MODE_ICON accepts a second argument, that allows
-//                     to change the char used as icon.
+//   setSelectionMode: Se the selection mode between AM_SELECTION_MODE_ICON and AM_SELECTION_MODE_INVERTED. AM_SELECTION_MODE_ICON accepts a second argument, that allows to change the char used as icon.
 //   setTextSize: Sets the menu text size (default 1).
 //
 // Class variables:
@@ -57,34 +50,34 @@ int whiteLedRangeOnExit(int8_t);
 // 
 
 // On Off Menu
-const char onOffMenu00 [] PROGMEM = { "On" };
-const char onOffMenu01 [] PROGMEM = { "Off"   };
-const char onOffMenu02 [] PROGMEM = { "Back"     };
+const char onOffMenu00 [] PROGMEM = { "On"   };
+const char onOffMenu01 [] PROGMEM = { "Off"  };
+const char onOffMenu02 [] PROGMEM = { "Back" };
 MENU_ITEM onOffMenu[]= {
-  // byte Type, char Text, void Function, MENU_ITEM SubItems, int rangeManage, bool toggleManage
-  { AM_ITEM_TYPE_COMMAND, onOffMenu00, redLedOn,  NULL, NULL, NULL},
-  { AM_ITEM_TYPE_COMMAND, onOffMenu01, redLedOff, NULL, NULL, NULL},
-  { AM_ITEM_TYPE_EOM,     onOffMenu02, NULL, NULL, NULL, NULL},
+  // byte Type, char Text, bool Function, MENU_ITEM SubItems, int rangeManage, bool toggleManage
+  { AM_ITEM_TYPE_COMMAND, onOffMenu00, redLedOn,  NULL, NULL, NULL },
+  { AM_ITEM_TYPE_COMMAND, onOffMenu01, redLedOff, NULL, NULL, NULL },
+  { AM_ITEM_TYPE_EOM,     onOffMenu02, NULL,      NULL, NULL, NULL },
 };
 
 
 // Main Menu
 // Also PROGMEM is required
-const char mainMenu00 [] PROGMEM = { "Main Menu"    };
-const char mainMenu01 [] PROGMEM = { "Red Led" };
-const char mainMenu02 [] PROGMEM = { "Green Led" };
-const char mainMenu03 [] PROGMEM = { "White Led"    };
-const char mainMenu04 [] PROGMEM = { "White Led OE"    };
-const char mainMenu05 [] PROGMEM = { "All Off"      };
+const char mainMenu00 [] PROGMEM = { "Main Menu"   };
+const char mainMenu01 [] PROGMEM = { "Red Led"     };
+const char mainMenu02 [] PROGMEM = { "Green Led"   };
+const char mainMenu03 [] PROGMEM = { "Blue Led"    };
+const char mainMenu04 [] PROGMEM = { "Blue Led OE" };
+const char mainMenu05 [] PROGMEM = { "All Off"     };
 
 MENU_ITEM mainMenu[] = {
-  // byte Type, char Text, void Function, MENU_ITEM SubItems, int rangeManage, bool toggleManage  
-  { AM_ITEM_TYPE_HEADER,  mainMenu00, NULL, NULL,    NULL, NULL},
-  { AM_ITEM_TYPE_MENU,    mainMenu01, NULL, onOffMenu, NULL, NULL},
-  { AM_ITEM_TYPE_TOGGLE,   mainMenu02, NULL, NULL,    NULL, greenLedToggle},
-  { AM_ITEM_TYPE_RANGE, mainMenu03, NULL, NULL,    whiteLedRange, NULL},
-  { AM_ITEM_TYPE_RANGE, mainMenu04, NULL, NULL,    whiteLedRangeOnExit, NULL},
-  { AM_ITEM_TYPE_EOM,     mainMenu05, allLedsOff, NULL,    NULL, NULL},
+  // byte Type, char Text, bool Function, MENU_ITEM SubItems, int rangeManage, bool toggleManage  
+  { AM_ITEM_TYPE_HEADER, mainMenu00, NULL,       NULL,      NULL,                NULL           },
+  { AM_ITEM_TYPE_MENU,   mainMenu01, NULL,       onOffMenu, NULL,                NULL           },
+  { AM_ITEM_TYPE_TOGGLE, mainMenu02, NULL,       NULL,      NULL,                greenLedToggle },
+  { AM_ITEM_TYPE_RANGE,  mainMenu03, NULL,       NULL,      whiteLedRange,       NULL           },
+  { AM_ITEM_TYPE_RANGE,  mainMenu04, NULL,       NULL,      whiteLedRangeOnExit, NULL           },
+  { AM_ITEM_TYPE_EOM,    mainMenu05, allLedsOff, NULL,      NULL,                NULL           },
 };
 
 
@@ -98,7 +91,7 @@ bool greenLedState = false;
 #define STEPS 10
 #define GREENLED 8
 #define REDLED 9
-#define WHITELED A0
+#define WHITELED 10
 
 void setup()
 {
@@ -117,35 +110,126 @@ void setup()
   // Draw the menu
   menuObject->drawMenu();
 
-  //delay(1000);
-  //menuObject->down();
-  //delay(1000);
-  //menuObject->enter(0, 15);
-  //delay(1000);
-  //menuObject->up(0, 15);
-  //delay(1000);
-  //menuObject->up(0, 15);
-  //delay(1000);
-  //delay(5000);
+  // Red led On/Off (Submenu)
+  delay(1000);
+  // Enter on submenu
+  menuObject->enter();
+  delay(1000);
+  // Select On function
+  menuObject->enter();
+  delay(1000);
+  // The return of the function is true, so we are in main menu again
+  // Enter into led menu again
+  menuObject->enter();
+  delay(1000);
+  // Go down to off option
+  menuObject->down();
+  delay(1000);
+  // Select the off option
+  menuObject->enter();
+  delay(1000);
+  // Now we are again on main menu again, setting the led to on again
+  menuObject->enter();
+  delay(1000);
+  menuObject->enter();
+
+  // Green led On/Off (Toggle)
+  delay(1000);
+  // Select the green led toggle
+  menuObject->down();
+  delay(1000);
+  // Turn it on
+  menuObject->enter();
+  delay(1000);
+  // Turn it off
+  menuObject->enter();
+  delay(1000);
+  // Turn it on again
+  menuObject->enter();
+
+  // Blue led dim in real time (Range)
+  // The callback function set to this menu item, always sets the
+  // led status, so changes are updated in real time.
+  delay(1000);
+  menuObject->down();
+  delay(1000);
+  // On this enter call we include the min and max range values to 
+  // show the meter.
+  menuObject->enter(0, 255);
+  delay(1000);
+  // Same when we want to change the value, to update the range meter in real time
+  for (int i = 0; i < 14; i++)
+  {
+    delay(500);
+    menuObject->up(0, 255);
+  }
+  for (int i = 0; i < 14; i++)
+  {
+    delay(500);
+    menuObject->down(0, 255);
+  }
+  // On exit call range is not required because the metter is hidden.
+  menuObject->enter();
+
+  // Blue led dim on set (Range)
+  // This function is similar to real time function, but this one only sets the led
+  // bright when 2 is received as argument (On range Exit).
+  delay(1000);
+  menuObject->down();
+  delay(1000);
+  menuObject->enter(0, 255);
+  delay(1000);
+  for (int i = 0; i < 14; i++)
+  {
+    delay(500);
+    menuObject->up(0, 255);
+  }
+  delay(1000);
+  // Sets the current value as acepted to call the function with 2 as argument
+  menuObject->enter();
+  delay(1000);
+  // Enter in range option to change it again
+  menuObject->enter(0, 255);
+  delay(1000);
+  for (int i = 0; i < 14; i++)
+  {
+    delay(500);
+    menuObject->down(0, 255);
+  }
+  delay(1000);
+  // As above, this enter will call the callback function with 2 as argument
+  menuObject->enter();
+
+  // Go down to exit function, which is called "All Off" in the example
+  // Yo can call a function to hide the menu for example...
+  delay(1000);
+  menuObject->down();
+  delay(1000);
+  menuObject->enter();
 }
 
 void loop() {
 }
 
-void redLedOn(MENU_ITEM item)
+bool redLedOn(MENU_ITEM item)
 {
   digitalWrite(REDLED, HIGH);
-  
+  // Return true makes the menu to go back 
+  return true;
 }
-void redLedOff(MENU_ITEM item)
+bool redLedOff(MENU_ITEM item)
 {
   digitalWrite(REDLED, LOW);
+  // Return true makes the menu to go back 
+  return true;
 }
-void allLedsOff(MENU_ITEM item)
+bool allLedsOff(MENU_ITEM item)
 {
   digitalWrite(GREENLED, LOW);
   digitalWrite(REDLED, LOW);
   analogWrite(WHITELED, 0);
+  // Return false keeps the current menu 
+  return false;
 }
 
 int whiteLedRange(int8_t mode)
@@ -162,7 +246,7 @@ int whiteLedRange(int8_t mode)
       whiteLedStatus = 0;
     }
   }
-  analogWrite(REDLED, whiteLedStatus);
+  analogWrite(WHITELED, whiteLedStatus);
   return whiteLedStatus;
 }
 
@@ -182,7 +266,7 @@ int whiteLedRangeOnExit(int8_t mode)
   }
   if (mode == 2)
   {
-    analogWrite(REDLED, whiteLedStatus);
+    analogWrite(WHITELED, whiteLedStatus);
   }
   return whiteLedStatus;
 }
